f02c10b4d886938638a909ca619a361cb8f9c5eb
huibosa_wsl <huibosa@gmail.com>
2021-12-05 14:54:50 +0800

    feat(NotebookUpdate.sh): add support to single file

---
+++ .scripts/NotebookUpdate.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+function usage() {
+  echo "Usage $0 [*.py || *.ipynb]"
+}
+
+function pyToIpynb() {
+  jupytext --to notebook "$@"
+}
+
+function ipynbToPy() {
+  jupytext --set-formats ipynb,py "$@"
+}
+
+function update() {
+  [[ "$1" == *.ipynb ]] && ipynbToPy $1
+  [[ "$1" == *.py ]] && pyToIpynb $1
+}
+
+function getRival() {
+  [[ $# -ne 1 ]] && echo "${FUNCNAME[0]}: Wrong argument count" && exit 1
+  if [[ "$1" == *.ipynb ]]; then
+    echo "${fname/%.ipynb/.py}" 
+  elif [[ "$1" == *.py ]]; then
+    echo "${fname/%.py/.ipynb}"
+  else
+    echo "${FUNCNAME[0]}: Wrong file format"
+    exit 1
+  fi
+}
+
+function updateAll() {
+  read -r -p "To convert all jupyter file recursively type \"CONVERT\": "
+  [[ "$REPLY" != "CONVERT" ]] && echo "hello"
+
+  readarray -d '' fnames < <(find ./ -type f \( -name '*.py' -o -name '*.ipynb' \) -print0)
+
+  for fname in "${fnames[@]}"; do
+    rival="$(getRival "$fname")"
+    
+    if [[ -f "$rival" ]]; then
+      if [[ "$fname" -nt "$rival" ]]; then
+        update "$fname"
+      elif [[ "$rival" -nt "$fname" ]]; then
+        update "$rival"
+      fi
+    else
+      update "$fname"
+    fi
+  done
+}
+
+
+if [[ $# -eq 0 ]]; then
+  updateAll
+elif [[ "$1" == *.ipynb ]]; then
+  jupytext --set-formats ipynb,py "$@"
+elif [[ "$1" == *.py ]]; then
+  jupytext --to notebook "$@"
+else
+  usage
+fi
