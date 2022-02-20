5447a3b948ff5b6b760ca4a2e11a474298019f47
huibosa_wsl <huibosa@gmail.com>
2022-02-20 15:55:05 +0800

    add go-directory

---
+++ .scripts/go-directory.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# all *.go file in current dir to a separate file
+
+dirName=""
+
+toDir() {
+  if [[ -e "$dirName" ]]; then
+    echo "Warning: directory <$dirName> already exist! Exiting..."
+    exit 1
+  else
+    mkdir "$dirName" && mv "$1" "$dirName"
+  fi
+}
+
+for f in *.go; do
+  dirName=${f%.*}
+  toDir "$f"
+done
