f1c11825788cb5a234b5b30a665427b68214354e
huibosa_wsl <huibosa@gmail.com>
2023-02-16 11:41:12 +0800

    modify gitignore file

---
+++ .scripts/pdf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+def catPdfs(fnames, output):
+    pdfWriter = PdfFileWriter()
+
+    for fname in fnames:
+        input = PdfFileReader(fname)
+        n = input.getNumPages()
+
+        for i in range(n):
+            pdfWriter.addPage(input.pages[i])
+
+    with open(output, "wb") as out:
+        pdfWriter.write(out)
