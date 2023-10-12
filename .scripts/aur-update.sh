2d057432edcc3001e62515f054ea97e94d7df5af
huibosa_macmini <huibosa@email.com>
2023-10-12 19:03:44 +0800

    chore: minor update

--- .scripts/aur-update.sh
+++ .scripts/aur-update.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 function usage() {
-	echo "Usage: $0 <package name>"
-	exit 1
+    echo "Usage: $0 <package name>"
+    exit 1
 }
 
 pack=
 path="$HOME/Downloads"
 
 case "$1" in
-  chrome) pack="google-chrome" ;;
-  code) pack="visual-studio-code-bin" ;;
-  wezterm) pack="wezterm-nightly-bin" ;;
-  *) usage ;;
+chrome) pack="google-chrome" ;;
+code) pack="visual-studio-code-bin" ;;
+wezterm) pack="wezterm-nightly-bin" ;;
+*) usage ;;
 esac
 
 (
-  cd "$path" || exit
+    cd "$path" || exit
 
-  if [[ -n "$pack" && -e "$pack" ]]; then
-    rm -rf "$pack"
-  fi
+    if [[ -n "$pack" && -e "$pack" ]]; then
+        rm -rf "$pack"
+    fi
 
-  git clone --depth 1 "https://aur.archlinux.org/${pack}.git" &&
-    cd "$path/$pack" &&
-    yes | makepkg -si &&
-    rm -rf $pack
+    git clone --depth 1 "https://aur.archlinux.org/${pack}.git" &&
+        cd "$path/$pack" &&
+        yes | makepkg -si &&
+        rm -rf $pack
 )
