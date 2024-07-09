abd7c48138034ad16fe493f29ccd229b18292fff
huibosa_macmini <huibosa@email.com>
2024-07-10 07:33:18 +0800

    chore

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -48,42 +48,56 @@ vim.g.loaded_pythonx_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
-vim.g.did_install_default_menus = 1
-vim.g.loaded_netrw = 1
-vim.g.netrw_liststyle = 3
-vim.g.loaded_sql_completion = 1
 
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
 vim.g.loaded_gzip = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_man = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_remote_plugins = 1
-vim.g.loaded_rplugin = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_shada_plugin = 1
-vim.g.loaded_spec = 1
-vim.g.loaded_spellfile_plugin = 1
-vim.g.loaded_spellfile_plugin = 1
 vim.g.loaded_tar = 1
 vim.g.loaded_tarPlugin = 1
-vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
 
-vim.g.load_black = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_rplugin = 1
+vim.g.loaded_shada_plugin = 1
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwSettings = 1
+
 vim.g.loaded_fzf = 1
+vim.g.loaded_fzf_vim = 1
+
+vim.g.loaded_tutor = 1
+vim.g.loaded_tutor_mode_plugin = 1
+
+vim.g.loaded_spellfile_plugin = 1
+
+vim.g.loaded_syntax = 1
+vim.g.loaded_synmenu = 1
+vim.g.loaded_optwin = 1
+vim.g.loaded_compiler = 1
+vim.g.loaded_bugreport = 1
+vim.g.loaded_ftplugin = 1
+
+vim.g.did_install_default_menus = 1
+vim.g.loaded_sql_completion = 1
+vim.g.loaded_spec = 1
+vim.g.loaded_man = 1
+vim.g.load_black = 1
 vim.g.loaded_gtags = 1
 vim.g.loaded_gtags_cscope = 1
-
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
 
 -- Create a new augroup to group the autocommands
 local augroup = vim.api.nvim_create_augroup("TextChangedGroup", { clear = true })
