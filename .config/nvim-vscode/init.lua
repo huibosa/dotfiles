abf9495a6e457dca7b099ddad6133c9f43c83e95
huibosa_macmini <huibosa@email.com>
2025-07-26 09:38:13 +0800

    chore(format): vscode

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -141,12 +141,12 @@ require("lazy").setup({
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         opts = {
             mappings = {
-                add = "gsa", -- Add surrounding in Normal and Visual modes
-                delete = "gsd", -- Delete surrounding
-                find = "gsf", -- Find surrounding (to the right)
-                find_left = "gsF", -- Find surrounding (to the left)
-                highlight = "gsh", -- Highlight surrounding
-                replace = "gsr", -- Replace surrounding
+                add = "gsa",            -- Add surrounding in Normal and Visual modes
+                delete = "gsd",         -- Delete surrounding
+                find = "gsf",           -- Find surrounding (to the right)
+                find_left = "gsF",      -- Find surrounding (to the left)
+                highlight = "gsh",      -- Highlight surrounding
+                replace = "gsr",        -- Replace surrounding
                 update_n_lines = "gsn", -- Update `n_lines`
             },
         },
