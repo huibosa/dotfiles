d7ee385b1e9a81801508693e55aa2f7d5ce64853
huibosa_win <huibosa@gmail.com>
2025-08-26 09:45:31 +0800

    feat: add wsl, powershell, windows terminal config

---
+++ .config/windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+
+$env:UV_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple"
+$env:UV_PYTHON_INSTALL_MIRROR = "https://pypi.tuna.tsinghua.edu.cn/simple"
+
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
