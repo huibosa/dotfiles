66043c76018be28c50db780b259bc9bd2419a68f
huibosa_macmini <huibosa@email.com>
2025-10-15 06:40:22 +0800

    BREAKING CHANGE: use treesitter main for nvim-vscode

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -21,9 +21,6 @@ if vim.g.vscode then
     keymap("n", "<SPACE>sS", function() vscode.action("workbench.action.showAllSymbols") end)
 end
 
-keymap({ "n", "x" }, "H", "^")
-keymap({ "n", "x" }, "L", "$")
-keymap("n", "c*", "*Ncgn")
 keymap("n", "J", "mzJ`z") -- Join next without moving cursor
 
 local opt = vim.opt
@@ -147,132 +144,123 @@ require("lazy").setup({
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                textobjects = {
-                    select = {
-                        enable = true,
-
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-
-                        keymaps = {
-                            ["a="] = { query = "@assignment.outer", desc = "outer assignment" },
-                            ["i="] = { query = "@assignment.inner", desc = "inner assignment" },
-
-                            ["aa"] = { query = "@parameter.outer", desc = "outer parameter" },
-                            ["ia"] = { query = "@parameter.inner", desc = "inner parameter" },
-
-                            ["aj"] = { query = "@conditional.outer", desc = "outer conditional" },
-                            ["ij"] = { query = "@conditional.inner", desc = "inner conditional" },
-
-                            ["ao"] = { query = "@loop.outer", desc = "outer loop" },
-                            ["io"] = { query = "@loop.inner", desc = "inner loop" },
-
-                            ["ab"] = { query = "@block.outer", desc = "outer block" },
-                            ["ib"] = { query = "@block.inner", desc = "inner block" },
-
-                            ["af"] = { query = "@function.outer", desc = "outer function" },
-                            ["if"] = { query = "@function.inner", desc = "inner function" },
-
-                            ["ac"] = { query = "@class.outer", desc = "outer class" },
-                            ["ic"] = { query = "@class.inner", desc = "inner class" },
-
-                            ["ar"] = { query = "@return.outer", desc = "outer return" },
-                            ["ir"] = { query = "@return.inner", desc = "inner return" },
-
-                            ["a/"] = { query = "@comment.outer", desc = "outer comment" },
-                            ["i/"] = { query = "@comment.inner", desc = "inner comment" },
-                        },
-                        include_surrounding_whitespace = true,
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]a"] = { query = "@parameter.outer", desc = "Next argument start" },
-                            ["]f"] = { query = "@function.outer", desc = "Next function start" },
-                            ["]r"] = { query = "@return.outer", desc = "Next return start" },
-                            ["]c"] = { query = "@class.outer", desc = "Next class start" },
-                            ["]j"] = { query = "@conditional.outer", desc = "Next judge start" },
-                            ["]o"] = { query = "@loop.outer", desc = "Next loop start" },
-                            ["]/"] = { query = "@comment.outer", desc = "Next comment start" },
-                        },
-                        goto_next_end = {
-                            ["]A"] = { query = "@parameter.outer", desc = "Next argument end" },
-                            ["]F"] = { query = "@function.outer", desc = "Next function end" },
-                            ["]R"] = { query = "@return.outer", desc = "Next return end" },
-                            ["]C"] = { query = "@class.outer", desc = "Next class end" },
-                            ["]J"] = { query = "@conditional.outer", desc = "Next judge end" },
-                            ["]O"] = { query = "@loop.outer", desc = "Next loop end" },
-                        },
-                        goto_previous_start = {
-                            ["[a"] = { query = "@parameter.outer", desc = "Previous argument start" },
-                            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-                            ["[r"] = { query = "@return.outer", desc = "Previous return start" },
-                            ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-                            ["[j"] = { query = "@conditional.outer", desc = "Previous judge start" },
-                            ["[o"] = { query = "@loop.outer", desc = "Previous loop start" },
-                            ["[/"] = { query = "@comment.outer", desc = "Next comment start" },
-                        },
-                        goto_previous_end = {
-                            ["[A"] = { query = "@parameter.outer", desc = "Previous argument end" },
-                            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-                            ["[R"] = { query = "@return.outer", desc = "Previous return end" },
-                            ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-                            ["[J"] = { query = "@conditional.outer", desc = "Previous judge end" },
-                            ["[O"] = { query = "@loop.outer", desc = "Previous loop end" },
-                        },
-                    },
+        event = { "BufReadPost", "BufNewFile" },
+        branch = "main",
+        opts = {
+            select = {
+                lookahead = true,
+                include_surrounding_whitespace = true,
+                selection_modes = {
+                    ["@parameter.outer"] = "v",
+                    ["@function.outer"] = "V",
+                    ["@class.outer"] = "<c-v>",
                 },
-            })
-            local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+            },
+            move = {
+                set_jumps = true,
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter-textobjects").setup(opts)
 
-            -- Repeat movement with ; and ,
-            vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-            vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+            -- Select
+            local select = require("nvim-treesitter-textobjects.select").select_textobject
+            local keymap = vim.keymap.set
+            local modes = { "x", "o" }
 
-            -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-            vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-            vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-            vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-            vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+            keymap(modes, "a=", function() select("@assignment.outer", "textobjects") end, { desc = "outer assignment" })
+            keymap(modes, "i=", function() select("@assignment.inner", "textobjects") end, { desc = "inner assignment" })
 
-            if vim.g.vscode then
-                local vscode = require("vscode-neovim")
+            keymap(modes, "aa", function() select("@parameter.outer", "textobjects") end, { desc = "outer parameter" })
+            keymap(modes, "ia", function() select("@parameter.inner", "textobjects") end, { desc = "inner parameter" })
 
-                -- Make ]h, [h also repeatable with ; and ,
-                local next_hunk = function() vscode.action("workbench.action.editor.nextChange") end
-                local prev_hunk = function() vscode.action("workbench.action.editor.previousChange") end
-                local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(next_hunk, prev_hunk)
-                keymap("n", "]h", next_hunk_repeat)
-                keymap("n", "[h", prev_hunk_repeat)
+            keymap(modes, "aj", function() select("@conditional.outer", "textobjects") end,
+                { desc = "outer conditional" })
+            keymap(modes, "ij", function() select("@conditional.inner", "textobjects") end,
+                { desc = "inner conditional" })
 
-                -- Make ]d, [d also repeatable with ; and ,
-                local next_diagnostic = function() vscode.action("editor.action.marker.next") end
-                local prev_diagnostic = function() vscode.action("editor.action.marker.prev") end
-                local next_diagnostic_repeat, prev_diagnostic_repeat =
-                    ts_repeat_move.make_repeatable_move_pair(next_diagnostic, prev_diagnostic)
-                keymap("n", "]d", next_diagnostic_repeat)
-                keymap("n", "[d", prev_diagnostic_repeat)
+            keymap(modes, "ao", function() select("@loop.outer", "textobjects") end, { desc = "outer loop" })
+            keymap(modes, "io", function() select("@loop.inner", "textobjects") end, { desc = "inner loop" })
 
-                -- Make ]r, [r also repeatable with ; and ,
-                local next_rf = function() vscode.action("editor.action.wordHighlight.next") end
-                local prev_rf = function() vscode.action("editor.action.wordHighlight.prev") end
-                local next_rf_repeat, prev_rf_repeat = ts_repeat_move.make_repeatable_move_pair(next_rf, prev_rf)
-                keymap("n", "]]", next_rf_repeat)
-                keymap("n", "[[", prev_rf_repeat)
-                vim.api.nvim_create_autocmd("FileType", {
-                    callback = function()
-                        local buffer = vim.api.nvim_get_current_buf()
-                        vim.keymap.set({ "n", "x", "o" }, "]]", next_rf_repeat,
-                            { desc = "Next Reference", buffer = buffer })
-                        vim.keymap.set({ "n", "x", "o" }, "[[", prev_rf_repeat,
-                            { desc = "Prev Reference", buffer = buffer })
-                    end,
-                })
-            end
+            keymap(modes, "ab", function() select("@block.outer", "textobjects") end, { desc = "outer block" })
+            keymap(modes, "ib", function() select("@block.inner", "textobjects") end, { desc = "inner block" })
+
+            keymap(modes, "af", function() select("@function.outer", "textobjects") end, { desc = "outer function" })
+            keymap(modes, "if", function() select("@function.inner", "textobjects") end, { desc = "inner function" })
+
+            keymap(modes, "ac", function() select("@class.outer", "textobjects") end, { desc = "outer class" })
+            keymap(modes, "ic", function() select("@class.inner", "textobjects") end, { desc = "inner class" })
+
+            keymap(modes, "ar", function() select("@return.outer", "textobjects") end, { desc = "outer return" })
+            keymap(modes, "ir", function() select("@return.inner", "textobjects") end, { desc = "inner return" })
+
+            keymap(modes, "a/", function() select("@comment.outer", "textobjects") end, { desc = "outer comment" })
+            keymap(modes, "i/", function() select("@comment.inner", "textobjects") end, { desc = "inner comment" })
+
+            -- Swap
+            local swap = require("nvim-treesitter-textobjects.swap")
+
+            vim.keymap.set("n", "<leader>csn", function() swap.swap_next("@parameter.inner") end,
+                { desc = "Swap with next param" })
+            vim.keymap.set("n", "<leader>csp", function() swap.swap_previous("@parameter.inner") end,
+                { desc = "Swap with prev param" })
+
+            -- Move
+            local move = require("nvim-treesitter-textobjects.move")
+            local move_modes = { "n", "x", "o" }
+
+            -- Goto next start
+            keymap(move_modes, "]a", function() move.goto_next_start("@parameter.outer", "textobjects") end,
+                { desc = "Next arg start" })
+            keymap(move_modes, "]f", function() move.goto_next_start("@function.outer", "textobjects") end,
+                { desc = "Next func start" })
+            keymap(move_modes, "]r", function() move.goto_next_start("@return.outer", "textobjects") end,
+                { desc = "Next return start" })
+            keymap(move_modes, "]c", function() move.goto_next_start("@class.outer", "textobjects") end,
+                { desc = "Next class start" })
+            keymap(move_modes, "]j", function() move.goto_next_start("@conditional.outer", "textobjects") end,
+                { desc = "Next cond start" })
+            keymap(move_modes, "]o", function() move.goto_next_start("@loop.outer", "textobjects") end,
+                { desc = "Next loop start" })
+            keymap(move_modes, "]/", function() move.goto_next_start("@comment.outer", "textobjects") end,
+                { desc = "Next loop start" })
+
+            -- Goto next end
+            keymap(move_modes, "]A", function() move.goto_next_end("@parameter.outer", "textobjects") end)
+            keymap(move_modes, "]F", function() move.goto_next_end("@function.outer", "textobjects") end)
+            keymap(move_modes, "]R", function() move.goto_next_end("@return.outer", "textobjects") end)
+            keymap(move_modes, "]C", function() move.goto_next_end("@class.outer", "textobjects") end)
+            keymap(move_modes, "]J", function() move.goto_next_end("@conditional.outer", "textobjects") end)
+            keymap(move_modes, "]O", function() move.goto_next_end("@loop.outer", "textobjects") end)
+
+            -- Goto previous start
+            keymap(move_modes, "[a", function() move.goto_previous_start("@parameter.outer", "textobjects") end)
+            keymap(move_modes, "[f", function() move.goto_previous_start("@function.outer", "textobjects") end)
+            keymap(move_modes, "[r", function() move.goto_previous_start("@return.outer", "textobjects") end)
+            keymap(move_modes, "[c", function() move.goto_previous_start("@class.outer", "textobjects") end)
+            keymap(move_modes, "[j", function() move.goto_previous_start("@conditional.outer", "textobjects") end)
+            keymap(move_modes, "[o", function() move.goto_previous_start("@loop.outer", "textobjects") end)
+            keymap(move_modes, "[/", function() move.goto_previous_start("@comment.outer", "textobjects") end)
+
+            -- Goto previous end
+            keymap(move_modes, "[A", function() move.goto_previous_end("@parameter.outer", "textobjects") end)
+            keymap(move_modes, "[F", function() move.goto_previous_end("@function.outer", "textobjects") end)
+            keymap(move_modes, "[R", function() move.goto_previous_end("@return.outer", "textobjects") end)
+            keymap(move_modes, "[C", function() move.goto_previous_end("@class.outer", "textobjects") end)
+            keymap(move_modes, "[J", function() move.goto_previous_end("@conditional.outer", "textobjects") end)
+            keymap(move_modes, "[O", function() move.goto_previous_end("@loop.outer", "textobjects") end)
+
+            local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+
+            -- Repeat movement with ; and , vim way
+            keymap(move_modes, ";", ts_repeat_move.repeat_last_move)
+            keymap(move_modes, ",", ts_repeat_move.repeat_last_move_opposite)
+
+            -- Make builtin f, F, t, T also repeatable with ; and ,
+            keymap(move_modes, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+            keymap(move_modes, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+            keymap(move_modes, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+            keymap(move_modes, "T", ts_repeat_move.builtin_T_expr, { expr = true })
         end,
     },
 })
