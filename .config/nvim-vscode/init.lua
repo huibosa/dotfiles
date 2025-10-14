a9b198bdfde16567d26cfd5410ed231c53b734a5
huibosa_macmini <huibosa@email.com>
2025-10-15 07:54:20 +0800

    fix(vscode): treesitter plugin

--- .config/nvim-vscode/init.lua
+++ .config/nvim-vscode/init.lua
@@ -129,6 +129,24 @@ require("lazy").setup({
         },
     },
     {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        branch = "main",
+        config = function()
+            require('nvim-treesitter').install({ 'rust', 'go', 'c', 'cpp', 'python' })
+
+            vim.opt.foldmethod = "expr"
+            vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+            -- Enable highlight
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = { 'python', 'go' },
+                callback = function() vim.treesitter.start() end,
+            })
+        end
+    },
+    {
         "nvim-treesitter/nvim-treesitter-textobjects",
         event = { "BufReadPost", "BufNewFile" },
         branch = "main",
